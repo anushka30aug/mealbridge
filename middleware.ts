1c327afbda30bd718e63b1119e5386f949abef36
anushka30aug <anushkashukla3003@gmail.com>
2025-03-12 14:15:05 +0530

    :zap: add jose for token verification

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
-import jwt from "jsonwebtoken";
+import { jwtVerify } from "jose";
 
-export function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest) {
   console.log("Middleware called");
+
+  async function verifyToken(token: string) {
+    try {
+      const { payload } = await jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET!));
+      return payload;
+    } catch (error) {
+      return null;
+    }
+  }
 
   const tokenFromUrl = req.nextUrl.searchParams.get("token");
   const tokenFromCookies = req.cookies.get("token")?.value;
@@ -12,45 +21,38 @@ export function middleware(req: NextRequest) {
   console.log("Token from URL:", tokenFromUrl);
   console.log("Token from Cookies:", tokenFromCookies);
 
-  // ✅ Set token from URL to cookies and redirect to /meals
   if (tokenFromUrl) {
     console.log("Setting token in cookies...");
-    const res = NextResponse.redirect(new URL("/meals", req.url));
+    const res = NextResponse.redirect(new URL("/", req.url));
     res.cookies.set("token", tokenFromUrl, {
       httpOnly: true,
-      secure: false, // Change to `true` in production
+      secure: false,
       sameSite: "strict",
-      maxAge: 7 * 24 * 60 * 60, // 7 days
+      maxAge: 7 * 24 * 60 * 60,
     });
     return res;
   }
 
-  // ✅ If token exists in cookies, verify it
   if (tokenFromCookies) {
-    try {
-      // jwt.verify(tokenFromCookies, process.env.JWT_SECRET!);
-      // console.log("Token is valid");
-
-      // 🚀 If user is on /signin but already authenticated, redirect to /meals
-      if (isOnSigninPage) {
-        return NextResponse.redirect(new URL("/meals", req.url));
-      }
-      return NextResponse.next(); // ✅ Allow access
-    } catch (err : unknown) {
-      console.error("Invalid token:");
+    let decoded = await verifyToken(tokenFromCookies);
+    console.log("Decoded -  ", decoded);
+    if (!decoded) {
       const res = NextResponse.redirect(new URL("/signin", req.url));
       res.cookies.delete("token");
       return res;
     }
+    if (isOnSigninPage) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+    return NextResponse.next();
   }
 
-  // 🛑 If no token and not on /signin, redirect to /signin
   if (!tokenFromCookies && !isOnSigninPage) {
     console.log("No token, redirecting to /signin");
     return NextResponse.redirect(new URL("/signin", req.url));
   }
 
-  return NextResponse.next(); // ✅ Allow access to /signin
+  return NextResponse.next();
 }
 
 export const config = {
