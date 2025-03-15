0bd1456c2af3b5812dd2c7a24fa8295bdc4e3384
Pratyush Namdev <pratyushnamdev140@gmail.com>
2025-03-15 16:39:56 +0530

    :zap: format code

--- app/(authentication)/signin/page.tsx
+++ app/(authentication)/signin/page.tsx
@@ -1,19 +1,19 @@
 import SignInComponent from "./partials/SignInComponent";
-import { FcGoogle } from "react-icons/fc";
+
 export default function SignIn() {
   return (
-      <div className="w-full h-full md:flex">
-        <div className="h-[80%] md:h-full md:w-1/2">
-          <video
-            autoPlay
-            muted
-            className="w-full h-full object-contain aspect-video"
-            src="/assets/signin.mp4"
-          />
-        </div>
-        <div className="h-[20%] md:h-full md:w-1/2 flex flex-col items-center text-center bg-transparent md:bg-[#005e38]">
-          <SignInComponent />
-        </div>
+    <div className="w-full h-full md:flex">
+      <div className="h-[80%] md:h-full md:w-1/2">
+        <video
+          autoPlay
+          muted
+          className="w-full h-full object-contain aspect-video"
+          src="/assets/signin.mp4"
+        />
       </div>
+      <div className="h-[20%] md:h-full md:w-1/2 flex flex-col items-center text-center bg-transparent md:bg-[#005e38]">
+        <SignInComponent />
+      </div>
+    </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,15 @@ import { useSocket } from "@/providers/socketProvider";
 
 export default function Home() {
   const { socket } = useSocket();
-  const test = () =>{
-    if(!socket) return ;
+  const test = () => {
+    if (!socket) return;
     socket.emit("ngo", "I'll take that food and donate it for you.");
-  }
+  };
   return (
-   <div><button onClick={test} className="bg-red-400 p-4 m-6 cursor-grab">NGO</button></div>
+    <div>
+      <button onClick={test} className="bg-red-400 p-4 m-6 cursor-grab">
+        NGO
+      </button>
+    </div>
   );
 }
