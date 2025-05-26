00d68e7235b5c956e69f8a497913ea736d87f606
anushka30aug <anushkashukla3003@gmail.com>
2025-05-26 22:03:44 +0530

    :zap: add socket event listeners of collector

--- providers/socketProvider.tsx
+++ providers/socketProvider.tsx
@@ -32,11 +32,27 @@ export default function SocketProvider({
         console.log("Emitted collector_connected", userId);
       }
     });
-    
+
     socketInstance.on("meal_reservation_cancelled_by_donor", (data) => {
-      console.log("GHJKGFHJKGFHJ");
-      console.log(data);
+      console.log("Donor has cancelled meal reservation");
+      alert(data.message);
     });
+
+    socketInstance.on("meal_cancelled", (data) => {
+      console.log("Donor has cancelled meal");
+      alert(data.message);
+    });
+
+    socketInstance.on("meal_expired", (data) => {
+      console.log("your reserved meal has expired");
+      alert(data.message);
+    });
+
+    socketInstance.on("meal_received", (data) => {
+      console.log("wohoo you have successfully received meal");
+      alert(data.message);
+    });
+
     socketInstance.on("catchdonor", (data) => {
       alert(data);
     });
